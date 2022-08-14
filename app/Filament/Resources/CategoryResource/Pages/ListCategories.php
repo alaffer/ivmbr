b4503a00f32dc0b2ec3f90376cdf263fd325eed1
Andreas Laffer <andreas.laffer@gmail.com>
2022-08-14 14:22:45 +0200

    namespaces correted

--- app/Filament/Resources/CategoryResource/Pages/ListCategories.php
+++ app/Filament/Resources/CategoryResource/Pages/ListCategories.php
@@ -2,11 +2,9 @@
 
 namespace App\Filament\Resources\CategoryResource\Pages;
 
-use Actions\Action;
-use Actions\CreateAction;
+use App\Filament\Resources\CategoryResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
-use App\Filament\Resources\CategoryResource;
 
 class ListCategories extends ListRecords
 {
@@ -15,8 +13,8 @@ class ListCategories extends ListRecords
     protected function getActions(): array
     {
         return [
-            CreateAction::make(),
-            Action::make('export')->label('Kategorien exportieren')->icon('heroicon-o-document-download')->url(route('category-export')),
+            Actions\CreateAction::make(),
+            Actions\Action::make('export')->label('Kategorien exportieren')->icon('heroicon-o-document-download')->url(route('category-export')),
         ];
     }
 }
