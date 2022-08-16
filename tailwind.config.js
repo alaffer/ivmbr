52308fbfd7646250167e7cf1b4d329f4b2a797c3
Andreas Laffer <andreas.laffer@gmail.com>
2022-08-16 09:25:44 +0200

    postcss+tailwind.config.js added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,43 @@
-/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors') 
+ 
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './resources/**/*.blade.php',
+        './vendor/filament/**/*.blade.php', 
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: { 
+                danger: colors.rose,
+                primary: colors.blue,
+                success: colors.green,
+                warning: colors.pink,
+            }, 
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'), 
+        require('@tailwindcss/typography'), 
+        plugin(function({ addComponents }) {
+            addComponents({
+                '.filament-button,.filament-tables-container': {
+                    borderRadius: '2px !important'
+                },
+                '.filament-sidebar-item > a' :{
+                    borderRadius: '2px !important'
+                },
+                '.filament-tables-pagination div' :{
+                    borderRadius: '2px !important'
+                },
+                '.ring-2' : {
+                    borderRadius: '2px !important'
+                },
+                'input,select' :{
+                    borderRadius: '2px !important'
+                }
+            })
+        })    ],
 }
