99058f3a307aac15828176dfcb854b76c5298070
Andreas Laffer <andreas.laffer@gmail.com>
2022-09-09 10:03:17 +0200

    added BankData-Policy

---
+++ app/Policies/BankDataPolicy.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\BankData;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class BankDataPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        return $user->role_id>=1;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Depot  $depot
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, BankData $bankData)
+    {
+        return $user->role_id>=1;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return $user->role_id==2;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Depot  $depot
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, BankData $bankData)
+    {
+        return $user->role_id==2;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Depot  $depot
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, BankData $bankData)
+    {
+        return $user->role_id==2;
+    }
+
+    /**
+     * Determine whether the user can delete the model via Bulk-menu.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Depot  $depot
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function deleteAny(User $user)
+    {
+        return $user->role_id==2;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Depot  $depot
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, BankData $bankData)
+    {
+        return $user->role_id==2;
+    }
+    public function restoreAny(User $user)
+    {
+        return $user->role_id==2;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Depot  $depot
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, BankData $bankData)
+    {
+        return $user->role_id==2;
+    }
+    public function forceDeleteAny(User $user)
+    {
+        return $user->role_id==2;
+    }
+}
